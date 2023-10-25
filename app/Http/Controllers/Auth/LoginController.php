573b08918d2670ddf61863e455f768cd2a28162c
Misbah Sohail <msbh.sohail@gmail.com>
2023-10-25 15:37:54 +0200

    created logging controllers to let the users log in using the credentials provided and logout

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        echo "please log in";
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            dd('invalid credentials ');
+        }
+
+        return redirect()->route('dashboard');
+    }
+}
