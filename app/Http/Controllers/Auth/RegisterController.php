dc5dbd80f388867168569d23b2a96a727193900f
Misbah Sohail <msbh.sohail@gmail.com>
2023-10-25 15:36:46 +0200

    created a register controller to create new users

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function store(Request $request)
+    {
+
+
+
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+
+        // creating user
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // signing in user
+        auth()->attempt($request->only('email', 'password'));
+
+        return redirect()->route('dashboard');
+
+
+
+
+
+        dd($request->email);
+    }
+}
